05d92abc902a3a4d5e5d26f0cfe50863c25148cb
bykj5262 <bykj5262@gmail.com>
2026-06-11 11:36:54 +0800

    Update ProxyParser.js

--- src/parsers/ProxyParser.js
+++ src/parsers/ProxyParser.js
@@ -4,10 +4,13 @@ import { parseVless } from './protocols/vlessParser.js';
 import { parseHysteria2 } from './protocols/hysteria2Parser.js';
 import { parseTrojan } from './protocols/trojanParser.js';
 import { parseTuic } from './protocols/tuicParser.js';
+import { parseSocks5 } from './protocols/socks5Parser.js';
 import { fetchSubscription } from './subscription/httpSubscriptionFetcher.js';
 
 const protocolParsers = {
     ss: parseShadowsocks,
+    socks5: parseSocks5,
+    socks: parseSocks5,   // 支持 socks:// 缩写
     vmess: parseVmess,
     vless: parseVless,
     hysteria: parseHysteria2,
